74c344e2f1de3b17ad2137e9f650921f86098caf
Joseph Budina <joebud3@gmail.com>
2021-05-13 22:36:11 -0600

    Add uban salary data

--- api.py
+++ api.py
@@ -1,19 +1,38 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/<string:location>')
+@app.route('/<location>')
 def index(location):
-  data = new_func(location)
+  data = get_urban_salary_data(location)
   return data
 
-def new_func(location):
-    req = requests.get('https://api.teleport.org/api/cities/?search=denver')
+def get_city_id(location):
+    # location = request.args['location']
+    # breakpoint()
+    params = {"search": location}
+    req = requests.get('https://api.teleport.org/api/cities', params=params)
     data = json.loads(req.content)
     return data['_embedded']['city:search-results'][0]['_links']['city:item']['href']
+
+def get_city_info(location):
+    # location = request.args['location']
+    req = requests.get(get_city_id(location))
+    data = json.loads(req.content)
+    return data
+
+def get_urban_data(location):
+    req = requests.get(get_city_info(location)['_links'][ "city:urban_area"]["href"])
+    data = json.loads(req.content)
+    return data
+
+def get_urban_salary_data(location):
+    req = requests.get(get_urban_data(location)['_links'][ "ua:salaries"]["href"])
+    data = json.loads(req.content)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
