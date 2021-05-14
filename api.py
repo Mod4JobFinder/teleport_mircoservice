fdac9e89064d12c51da2e8046e62c4fd9bc89e60
Joseph Budina <joebud3@gmail.com>
2021-05-14 14:31:15 -0600

    Refactor

--- api.py
+++ api.py
@@ -1,38 +1,20 @@
-from flask import Flask, request
+from flask import Flask
 import requests
 import json
 
 app = Flask(__name__)
 
-
-@app.route('/<location>')
+@app.route('/api/<location>')
 def index(location):
   data = get_urban_salary_data(location)
   return data
 
-def get_city_id(location):
-    # location = request.args['location']
-    # breakpoint()
-    params = {"search": location}
-    req = requests.get('https://api.teleport.org/api/cities', params=params)
-    data = json.loads(req.content)
-    return data['_embedded']['city:search-results'][0]['_links']['city:item']['href']
-
-def get_city_info(location):
-    # location = request.args['location']
-    req = requests.get(get_city_id(location))
-    data = json.loads(req.content)
-    return data
-
-def get_urban_data(location):
-    req = requests.get(get_city_info(location)['_links'][ "city:urban_area"]["href"])
-    data = json.loads(req.content)
-    return data
-
 def get_urban_salary_data(location):
-    req = requests.get(get_urban_data(location)['_links'][ "ua:salaries"]["href"])
-    data = json.loads(req.content)
-    return data
+  params = {"slug:": location}
+  req = requests.get("https://api.teleport.org/api/urban_areas/slug:%s/salaries" % location)
+  data = json.loads(req.content)
+  salaries = {"salaries": data["salaries"]}
+  return salaries
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
